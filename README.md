a7d02a7268f0773d2fca7d95ddd5ffeea2e002e2
Barry Walsh <draegtun@googlemail.com>
2015-02-23 15:27:39 +0000

    bang

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+PerlX::QuoteOperator::URL - Blah blah blah
+
+# SYNOPSIS
+
+    use PerlX::QuoteOperator::URL;
+
+# DESCRIPTION
+
+PerlX::QuoteOperator::URL is
+
+# AUTHOR
+
+Barry Walsh <draegtun@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2015- Barry Walsh
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/PerlX/QuoteOperator/URL.pm
@@ -0,0 +1,39 @@
+package PerlX::QuoteOperator::URL;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlX::QuoteOperator::URL - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use PerlX::QuoteOperator::URL;
+
+=head1 DESCRIPTION
+
+PerlX::QuoteOperator::URL is
+
+=head1 AUTHOR
+
+Barry Walsh E<lt>draegtun@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Barry Walsh
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
